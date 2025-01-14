5cb635672e46c57ee9cf954fba3a09af7140f43e
ilovelinux774 <yedekyesildal@gmail.com>
2025-01-14 10:48:23 +0300

    Update updateallapps.bat

--- updateallapps.bat
+++ updateallapps.bat
@@ -1,15 +1,1 @@
-readme.txt
-echo Please read the readme text document before running the application
-pause
-echo If you have time to update, press any key to countinue
-echo The application was written by Yasin
-echo subscribe to me click the link http://www.youtube.com/@WINDOWS74090
-echo This program is still in developing stage so something can go wrong so dont complain us or run this app if you dont trust, or dont have enough storage
-echo we cant calculate the upgrade size of it we will try to fix that too
-pause
-winget upgrade --all
-echo checking for unable to updated apps this will take a minute or more then a minute if therese more unable to updated programs
-winget upgrade --include-unknown
-echo Checked for updates/updated you can close this window
-echo if you want this app to open when logged in copy the bat file to startup file
-pause 
+
